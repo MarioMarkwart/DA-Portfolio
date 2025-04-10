e0d33c73071e829a6ff5b6a49a467dc41aa00c8f
mario.markwart <mario.markwart@gmail.com>
2025-04-10 19:29:06 +0000

    button component

---
+++ da-portfolio/src/app/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() label: string = 'Button';
+  @Input() type: 'button' | 'submit' = 'button';
+  @Output() clicked = new EventEmitter<void>();
+
+  handleClick() {
+    this.clicked.emit();
+  }
+}
--- da-portfolio/src/app/hero/hero.component.ts
+++ da-portfolio/src/app/hero/hero.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
 	selector: 'app-hero',
-	imports: [],
+	imports: [ButtonComponent],
 	templateUrl: './hero.component.html',
 	styleUrl: './hero.component.scss',
 })
-export class HeroComponent {}
+export class HeroComponent {
+
+	checkWork()	{
+		console.log('check work');
+	}
+
+	contact()	{
+		console.log('contact');
+	}
+}
