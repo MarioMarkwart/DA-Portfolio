ee6a654d6f059bb5841e66357dadba3365d4a369
mario.markwart <mario.markwart@gmail.com>
2025-04-14 13:00:44 +0000

    marquee

--- da-portfolio/src/app/hero/hero.component.ts
+++ da-portfolio/src/app/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../button/button.component";
 import { MarqueeComponent } from "./marquee/marquee.component";
+import { SkillsetComponent } from "../skillset/skillset.component";
 
 @Component({
 	selector: 'app-hero',
