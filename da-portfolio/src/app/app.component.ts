9f2357467f68e0b6a15336714a55822eba5807c1
Mario Markwart <mario.markwart@gmail.com>
2025-05-19 19:39:52 +0200

    testemonials [{...}]

--- da-portfolio/src/app/app.component.ts
+++ da-portfolio/src/app/app.component.ts
@@ -3,16 +3,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
 import { AboutComponent } from "./about/about.component";
-import { InfoboxComponent } from "./shared/infobox/infobox.component";
-import { SkillItemComponent } from "./skills/skill-item/skill-item.component";
 import { SkillsetComponent } from "./skillset/skillset.component";
 import { ProjectsComponent } from "./projects/projects.component"
+import { TestimonialsComponent } from "./testimonials/testimonials.component";
+
 
 
 @Component({
   standalone:true,
 	selector: 'app-root',
-	imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, InfoboxComponent, SkillItemComponent, SkillsetComponent, ProjectsComponent],
+	imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, SkillsetComponent, ProjectsComponent, TestimonialsComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
--- da-portfolio/src/app/testimonials/testimonials.component.ts
+++ da-portfolio/src/app/testimonials/testimonials.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 	templateUrl: './testimonials.component.html',
 	styleUrl: './testimonials.component.scss',
 })
-export class TestimonialsComponent {}
+export class TestimonialsComponent {
+
+	constructor() { }
+	testemonials = [
+		{
+			name: 'John Doe',
+			relation: 'Former Colleague',
+			feedback: 'This is a great service! Highly recommend.',
+		},
+		{
+			name: "Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.",
+			relation: "Team Partner",
+			feedback: "I had the good fortune of working with Lukas in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He's super knowledgeable, easy to work with, and I'd happily work with him again given the chance.",
+		},
+		{
+			name: "J. Schmitt",
+			relation: "Frontend Developer",
+			feedback: "Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project.",
+		}
+	]
+
+
+}
