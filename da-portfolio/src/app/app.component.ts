7801f29f699c26e28763f11d7466b9effa6861ab
mario.markwart <mario.markwart@gmail.com>
2025-10-29 20:24:10 +0000

    footer link hover

--- da-portfolio/src/app/app.component.ts
+++ da-portfolio/src/app/app.component.ts
@@ -2,18 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HeroComponent } from './hero/hero.component';
-import { AboutComponent } from "./about/about.component";
-import { SkillsetComponent } from "./skillset/skillset.component";
-import { ProjectsComponent } from "./projects/projects.component"
-import { TestimonialsComponent } from "./testimonials/testimonials.component";
-import { ContactComponent } from "./contact/contact.component";
-
-
+import { AboutComponent } from './about/about.component';
+import { SkillsetComponent } from './skillset/skillset.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
-  standalone:true,
+	standalone: true,
 	selector: 'app-root',
-	imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutComponent, SkillsetComponent, ProjectsComponent, TestimonialsComponent, ContactComponent],
+	imports: [
+		RouterOutlet,
+		HeaderComponent,
+		HeroComponent,
+		AboutComponent,
+		SkillsetComponent,
+		ProjectsComponent,
+		TestimonialsComponent,
+		ContactComponent,
+		FooterComponent
+	],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
